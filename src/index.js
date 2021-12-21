98e08ad01d94f90c120cc6214129ab6acc1eac71
parashar-aakash <aakash.parashar@successive.tech>
2021-12-21 22:37:33 +0530

    Feat: Project Set-up

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://aman:aman@cluster0.yd8i9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+.then(console.log('db connected'))
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/uploads', express.static('/uploads'));
+
+app.use('/uploads*', (req, res, next) => {
+    try {
+      res.sendFile(__dirname + '/uploads' + req.params[0]);
+      res.send('working');
+  
+    } catch (error) {
+      next();
+    }
+  });
+
+  app.listen(PORT, (err) => {
+    if (err) {
+    console.log(err);
+    }
+    console.log(`App is running ${PORT}`);
+});
